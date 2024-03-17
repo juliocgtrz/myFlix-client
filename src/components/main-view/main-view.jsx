7ad46974c9cd2bf32ee33410a20e47e458fc168a
Julio G <juliocgtrz@gmail.com>
2024-03-17 15:36:07 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,7 +12,7 @@ export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
     const storedToken = localStorage.getItem("token");
     const [user, setUser] = useState(storedUser? storedUser : null);
-    const [token, setToken] = useState(storedToken? storedToken : null);
+    const [token, setToken] = useState(storedToken? storedToken : null); 
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
