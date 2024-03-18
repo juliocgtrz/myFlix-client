500d686fb824f37466ccfcc38b47566cf2a4c0f6
Julio G <juliocgtrz@gmail.com>
2024-03-17 20:56:07 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -25,7 +25,7 @@ export const MainView = () => {
         })
             .then((response) => response.json())
             .then((data) => {
-                const moviesFromApi = movie.map((movie) => {
+                const moviesFromApi = data.map((movie) => {
                     return {
                         id: movie._id,
                         image: movie.ImagePath,
