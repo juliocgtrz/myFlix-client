4eab0bffbdcf19ba228d9463f0872acc8a118f53
Julio G <juliocgtrz@gmail.com>
2024-03-27 11:45:26 -0600

    Update user-info.jsx
    added proptypes

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const UserInfo = ({email, name}) => {
     return (
@@ -7,4 +8,9 @@ export const UserInfo = ({email, name}) => {
             <p>Email: {email} </p>
         </div>
     );
+};
+
+UserInfo.PropTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired
 };
