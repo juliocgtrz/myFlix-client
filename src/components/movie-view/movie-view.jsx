e27cb4a105387f0706786272fb8ddb6e0a612c37
Julio G <juliocgtrz@gmail.com>
2024-03-27 22:34:40 -0600

    Update movie-view.jsx
    imported proptypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
