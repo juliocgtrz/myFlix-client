5f0df2aeace36065643238262c72be46ccf84c49
Julio G <juliocgtrz@gmail.com>
2024-03-31 21:11:25 -0600

    Update main-view.jsx
    imported reducers, movieslist, refactored code to use redux for search and movie list

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,74 +13,51 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { ProfileView } from "../profile-view/profile-view";
+import { useSelector, useDispatch } from "react-redux";
+import { setMovies } from "../../redux/reducers/movies";
+import { setUser } from "../../redux/reducers/user/user";
 import { MoviesList } from "../movies-list";
 
-
 export const MainView = () => {
-    //Redux
-    const movies = useSelector((state) => state.movies.data);
-    const moviesStatus = useSelector((state) => state.movies.status);
-    const moviesError = useSelector((state) => state.movies.error);
-    const user = useSelector((state) => state.user.userData);
-    const token = useSelector((state) => state.user.token);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const movies = useSelector((state) => state.movies.list);
+    const user = useSelector((state) => state.user);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const dispatch = useDispatch();
-
-    const safelyParseJSON = (json) => {
-        try {
-            return JSON.parse(json);
-        } catch (error) {
-            console.error("Parsing error on ", json, error);
-            return null;
-        }
-    };
 
     //Load data from API
     useEffect(() => {
-        //If user and token are saved in local storage the user does not have to login again
-        const savedUser = safelyParseJSON(localStorage.getItem("user"));
-        const savedToken = localStorage.getItem("token");
+        if (!token) {
+            return;
+        }    
 
-        if (!user && savedUser) {
-            dispatch(setUserData(savedUser));
-        }
-        if (!token && savedToken) {
-            dispatch(setToken(savedToken));
-        }
-        if (savedToken && !movies?.length) {
-            dispatch(fetchMovies(savedToken));
-        }
-    }, [user, token, movies?.length, dispatch]);
-
-    if (moviesStatus === "loading") {
-        return (
-            <Container>
-                <Row>
-                    <Col className="d-flex justify-content-center align-items-center">
-                        <Spinner animation="border" role="status">
-                            <span className="visually-hidden">Loading..</span>
-                        </Spinner>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    };
-
-    if (moviesStatus === "failed") {
-        return (
-            <Container>
-                <Row>
-                    <Col className="d-flex justify-content-center align-items-center">
-                        <p>Error fetching movies: {moviesError}</p>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    };
+        fetch("https://my-movies-flix-db-60666e043a4b.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Movies data: ", data);
+                const moviesFromApi = data.map((movie) => {
+                    return {
+                        id: movie._id,
+                        image: movie.ImagePath,
+                        title: movie.Title,
+                        description: movie.Description,
+                        genre: movie.Genre.Name,
+                        director: movie.Director.Name,
+                        featured: movie.featured,
+                    };
+                });
+                localStorage.setItem("movies", JSON.stringify(moviesFromApi));
+                dispatch(setMovies(moviesFromApi));
+            });
+    }, []);
 
     return (
         <BrowserRouter>
             <NavigationBar />
-            <Container className="mt-5">
+            <Row className="justify-content-md-center">
                 <Routes>
                     <Route path="/signup" element={
                         user ?
@@ -91,22 +68,53 @@ export const MainView = () => {
                     <Route
                         path="/movies"
                         element={
-                            !user ? <Navigate to="/login" replace /> :
-                                movies?.length === 0 ?
-                                    <Col><h2 classname="my-4">No movies to display.</h2></Col> :
-                                    <MoviesList />
+                            <>
+                                {user ? (
+                                    <Navigate to="/" />
+                                ) : (
+                                    <Col md={5}>
+                                        <SignupView />
+                                    </Col>
+                                )}
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <>
+                                {user ? (
+                                    <Navigate to="/" />
+                                ) : (
+                                    <Col md={5}>
+                                        <LoginView />
+                                    </Col>
+                                )}
+                            </>
                         }
                     />
                     <Route
                         path="/movies/:movieId"
-                        element={!user ? <Navigate to="/login" replace /> : movies?.length === 0 ?
-                            <Col>No movies to display.</Col> :
-                            <MovieView />}
+                        element={
+                            <>
+                                {!user ? (
+                                    <Navigate to="/login" replace />
+                                ) : movies.length === 0 ? (
+                                    <Col>The list is empty!</Col>
+                                ) : (
+                                    <Col md={8}>
+                                        <MovieView />
+                                    </Col>
+                                )}
+                            </>
+                        }
                     />
                     <Route
                         path="/"
-                        element={user ? <Navigate to="/movies" replace /> :
-                            <Navigate to="/login" replace />}
+                        element={
+                            <>
+                                {!user ? <Navigate to="/login" replace /> : <MoviesList />}</>
+                        }
                     />
                     <Route path="/profile"
                         element={
