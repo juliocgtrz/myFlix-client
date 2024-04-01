1b8dd4b75f1b069a490459d969ee228c177cbac8
Julio G <juliocgtrz@gmail.com>
2024-03-31 21:10:47 -0600

    Update login-view.jsx
    updated code to use redux dispatch

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -28,7 +28,7 @@ export const LoginView = ({ onLoggedIn }) => {
             if (data.user) {
                 localStorage.setItem("user", JSON.stringify(data.user));
                 localStorage.setItem("token", data.token);
-                onLoggedIn(data.user, data.token);
+                dispatch(setUser(data.user, data.token));
             } else {
                 alert("No such user");
             }
