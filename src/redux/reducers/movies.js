5e039e97ba842b51eb2f6d41153a22eea6ed4d52
Julio G <juliocgtrz@gmail.com>
2024-04-04 00:17:35 -0600

    Update movies.js
    updating code to use redux

--- src/redux/reducers/movies.js
+++ src/redux/reducers/movies.js
@@ -1,10 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchMovies = createAsyncThunk(
+    "movies/fetchMovies",
+    async (token, { rejectWithValue }) => {
+        try {
+            const response = await fetch("https://my-movies-flix-db-60666e043a4b.herokuapp.com/movies", {
+                headers: { Authorization: `Bearer ${token}`},
+            });
+            if (!response.ok) {
+                throw new Error("Failed to fetch movies");
+            }
+            const data = await response.json();
+            return data.map((movie) => ({
+                id: movie._id,
+                image: movie.ImagePath,
+                title: movie.Title,
+                description: movie.Description,
+                genre: movie.Genre.Name,
+                director: movie.Director.Name,
+            }));
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
 
 const moviesSlice = createSlice ({
     name: "movies",
     initialState: {
-        list: [],
-        filter: ""
+        data: [],
+        filter: "",
+        error: null,
     },
     reducers: {
         setMovies: (state, action) => {
@@ -16,5 +42,6 @@ const moviesSlice = createSlice ({
     }
 });
 
+//Exporting reducers
 export const { setMovies, setFilter } = moviesSlice.actions;
 export default moviesSlice.reducer;
