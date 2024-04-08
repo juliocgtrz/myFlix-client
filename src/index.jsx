3d469ac49361adff97d0e323506328a74b1e670f
Julio G <juliocgtrz@gmail.com>
2024-04-07 22:53:57 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import { Container } from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
