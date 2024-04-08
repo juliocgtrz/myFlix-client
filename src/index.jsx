720767615dc859b67a18f35401331ed5297b48fe
Julio G <juliocgtrz@gmail.com>
2024-04-07 21:48:31 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,8 @@ import { Container } from "react-bootstrap/Container";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap.min.css";
 //Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
